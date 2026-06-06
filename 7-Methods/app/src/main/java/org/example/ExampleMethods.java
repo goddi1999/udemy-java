aa8d35644892db50cb38d30257f4f1a4fce7ec72
Godfred Amoah Sefa <godfredasefa@icloud.com>
2026-06-06 16:22:01 +0200

    add new methods to project

---
+++ 7-Methods/app/src/main/java/org/example/ExampleMethods.java
@@ -0,0 +1,16 @@
+
+package org.example;
+
+public class ExampleMethods {
+
+
+public static void main(String[] args) {
+    
+    log();
+}
+
+public static void log(){
+
+        System.out.println("log exampleMethods");
+    }
+}
