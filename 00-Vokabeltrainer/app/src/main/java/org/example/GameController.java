59588d0cc0c90f4e0010ea08726bd822a7d8be36
Godfred Amoah Sefa <godfredasefa@icloud.com>
2026-06-08 15:23:46 +0200

    tied fxml to contorller

--- 00-Vokabeltrainer/app/src/main/java/org/example/GameController.java
+++ 00-Vokabeltrainer/app/src/main/java/org/example/GameController.java
@@ -1,6 +1,16 @@
 package org.example;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class GameController {
+    public Button submitButton;
+    public Label submitLabel;
 
+    public void handleButtonClick(){
+        System.out.println("button is clicked");
+        submitButton.setText("text has changed");
+        submitLabel.setText("Hello godfred");
 
+    }
 }
